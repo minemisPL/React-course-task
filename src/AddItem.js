6ba2ec3c50a11eadbdaa775af32be3ef5698e286
minemisPL <typowonietypowegm@gmail.com>
2021-11-23 12:09:32 +0100

    Everything

---
+++ src/AddItem.js
@@ -0,0 +1,31 @@
+import { FaPlus } from "react-icons/all";
+import { useRef } from "react";
+
+const AddItem = ({ newItem, setNewItem, handleSubmit }) => {
+    const inputRef = useRef()
+
+    return (
+        <form className={'addForm'} onSubmit={handleSubmit}>
+            <label htmlFor="addItem">Add Item</label>
+            <input
+                autoFocus
+                ref={inputRef}
+                type="text"
+                placeholder={'Add Item'}
+                required
+                value={newItem}
+                onChange={(event => setNewItem(event.target.value))}
+            />
+
+            <button
+                type={"submit"}
+                aria-label={'Add Item'}
+                onClick={() => inputRef.current.focus()}
+            >
+                <FaPlus />
+            </button>
+        </form>
+    );
+};
+
+export default AddItem;
--- src/App.js
+++ src/App.js
@@ -1,23 +1,139 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from "./Header";
+import Content from "./Content";
+import Footer from "./Footer";
+import { useState, useEffect } from "react";
+import AddItem from "./AddItem";
+import SearchItem from "./SearchItem";
+import apiRequest from "./apiRequest";
 
 function App() {
+    const API_URL = "http://localhost:3500/items"
+
+    const [items, setItems] = useState([])
+    const [newItem, setNewItem] = useState('')
+    const [search, setSearch] = useState('')
+    const [isLoading, setIsLoading] = useState(true)
+    const [fetchError, setFetchError] = useState(null)
+
+    useEffect(() => {
+
+        const fetchItems = async () => {
+            try {
+                const response = await fetch(API_URL)
+                if (!response.ok) {
+                    setItems([])
+                    throw Error("Did not received expected data")
+                }
+                const listItems = await response.json()
+                setItems(listItems)
+            } catch (err) {
+                console.error(err.stack)
+            } finally {
+                setIsLoading(false)
+            }
+        }
+
+        setTimeout(() => {
+            fetchItems()
+        }, 0)
+    }, [])
+
+    const handleCheck = async (id) => {
+        const listItems = items.map((item) =>
+            item.id === id
+                ? {...item, checked: !item.checked}
+                : item)
+
+        setItems(listItems)
+
+        const myItem = listItems.filter(item => item.id === id)
+        const updateOption = {
+            method: 'PATCH',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ checked: myItem[0].checked })
+        }
+
+        const reqUrl = `${API_URL}/${id}`
+        const result = await apiRequest(reqUrl, updateOption)
+        if (result) setFetchError(result)
+    }
+
+    const handleDelete = async (id) => {
+        const listItems = items.filter((item) => item.id !== id)
+        setItems(listItems)
+
+        const deleteOptions = {
+            method: 'DELETE'
+        }
+
+        const reqUrl = `${API_URL}/${id}`
+        const result = await apiRequest(reqUrl, deleteOptions)
+        if (result) setFetchError(result)
+    }
+    
+    const handleSubmit = (event) => {
+        event.preventDefault()
+        if (!newItem) return
+
+        addItem(newItem)
+
+        setNewItem('')
+    }
+
+    const addItem = async (item) => {
+        const id = items.length
+            ? items[items.length - 1].id + 1
+            : 1
+
+        const myNewItem = {id, checked: false, item}
+
+        const listItems = [...items, myNewItem]
+
+        setItems(listItems)
+
+        const postOptions = {
+            method: 'Post',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(myNewItem)
+        }
+
+        const result = await apiRequest(API_URL, postOptions)
+
+        if (result) setFetchError(result)
+    }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <Header />
+        <AddItem
+            newItem={newItem}
+            setNewItem={setNewItem}
+            handleSubmit={handleSubmit}
+        />
+        <SearchItem
+            search={search}
+            setSearch={setSearch}
+        />
+        <main>
+            {isLoading && <p>Loading Items...</p>}
+            {fetchError && <p style={{ color: "red" }}>{`Error: ${fetchError}`}</p>}
+            {!isLoading &&
+            <Content
+                items = {items.filter(item =>
+                    ((item.item)
+                        .toLowerCase()
+                        .includes(search.toLowerCase())))
+                }
+                handleCheck = {handleCheck}
+                handleDelete = {handleDelete}
+            />}
+        </main>
+
+        <Footer length ={items.length}/>
     </div>
   );
 }
